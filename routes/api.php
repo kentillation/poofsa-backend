caead7205a1c35d4e96e306b9998056588c00af0
Kent Anthony Engbino <kentanthony070495@gmail.com>
2026-04-04 17:22:07 +0800

    Updated

--- routes/api.php
+++ routes/api.php
@@ -28,9 +28,9 @@ use App\Models\DevModel;
 // Public
 Route::post('v1/public/save-shop', [PublicController::class, 'saveShop']);
 // Route::get('v1/public/shops', [PublicController::class, 'getShops']);
-Route::get('v1/public/shops', [PublicController::class, 'getAllPublicShops']); //new structure
+Route::get('v1/public/all-shops', [PublicController::class, 'getAllPublicShops']); //new structure
 // Route::get('v1/public/products', [PublicController::class, 'getProducts']);
-Route::get('v1/public/products', [PublicController::class, 'getAllPublicProducts']); //new structure
+Route::get('v1/public/all-products', [PublicController::class, 'getAllPublicProducts']); //new structure
 Route::get('v1/public/new-products', [PublicController::class, 'getNewProducts']);
 Route::get('v1/public/categories-by-new-products', [PublicController::class, 'getCategoriesByNewProducts']);
 Route::get('v1/public/categories-by-meal-type', [PublicController::class, 'getCategoriesByMealType']);
